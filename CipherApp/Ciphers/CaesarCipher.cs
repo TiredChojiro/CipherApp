09a8bb389102960f82eac6cba4f8b82449f12b4e
Dima <dchojiro@gmail.com>
2024-03-14 02:29:57 +0600

    Add project files.

---
+++ CipherApp/Ciphers/CaesarCipher.cs
@@ -0,0 +1,72 @@
+﻿using System.Text;
+
+namespace CipherApp.Ciphers
+{
+    public class CaesarCipher
+    {
+        private int shift;
+
+        public CaesarCipher(int shift)
+        {
+            this.shift = shift;
+        }
+
+        public static string Encrypt(string text, int shift)
+        {
+
+            if (shift < 1 || shift > 25)
+            {
+                throw new ArgumentException("Shift value must be between 1 and 25.");
+            }
+
+            StringBuilder result = new StringBuilder();
+
+            foreach (char character in text)
+            {
+                if (char.IsUpper(character))
+                {
+                    result.Append((char)(((character - 'A') + shift) % 26 + 'A'));
+                }
+                else if (char.IsLower(character))
+                {
+                    result.Append((char)(((character - 'a') + shift) % 26 + 'a'));
+                }
+                else
+                {
+                    result.Append(character);
+                }
+            }
+
+            return result.ToString();
+        }
+
+        public static string Decrypt(string text, int shift)
+        {
+
+            if (shift < 1 || shift > 25)
+            {
+                throw new ArgumentException("Shift value must be between 1 and 25.");
+            }
+
+            StringBuilder result = new StringBuilder();
+
+            foreach (char character in text)
+            {
+                if (char.IsUpper(character))
+                {
+                    result.Append((char)(((character - 'A') - shift + 26) % 26 + 'A'));
+                }
+                else if (char.IsLower(character))
+                {
+                    result.Append((char)(((character - 'a') - shift + 26) % 26 + 'a'));
+                }
+                else
+                {
+                    result.Append(character);
+                }
+            }
+
+            return result.ToString();
+        }
+    }
+}
