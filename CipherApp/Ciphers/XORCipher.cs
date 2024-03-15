239f0d29f9ddf564a36777fe8d6c2aa267b5672d
Dima <dchojiro@gmail.com>
2024-03-15 21:05:19 +0600

    Adding XORCipher & updating ui

---
+++ CipherApp/Ciphers/XORCipher.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+
+namespace CipherApp.Ciphers
+{
+    public class XORCipher
+    {
+        public static string Encrypt(string text, string keyword)
+        {
+            byte[] inputBytes = Encoding.UTF8.GetBytes(text);
+            byte[] keyBytes = Encoding.UTF8.GetBytes(keyword);
+            byte[] outputBytes = new byte[inputBytes.Length];
+
+            for (int i = 0; i < inputBytes.Length; i++)
+            {
+                outputBytes[i] = (byte)(inputBytes[i] ^ keyBytes[i % keyBytes.Length]);
+            }
+
+            return Convert.ToBase64String(outputBytes);
+        }
+
+        public static string Decrypt(string text, string keyword)
+        {
+            byte[] inputBytes = Convert.FromBase64String(text);
+            byte[] keyBytes = Encoding.UTF8.GetBytes(keyword);
+            byte[] outputBytes = new byte[inputBytes.Length];
+
+            for (int i = 0; i < inputBytes.Length; i++)
+            {
+                outputBytes[i] = (byte)(inputBytes[i] ^ keyBytes[i % keyBytes.Length]);
+            }
+
+            return Encoding.UTF8.GetString(outputBytes);
+        }
+    }
+}
